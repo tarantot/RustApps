0d3eadb849b63fa7e79133d3f4389d1c631be15d
tarantot <roskishnyi@gmail.com>
2024-09-16 18:44:23 +0300

    Rust applications

---
+++ command_line_app/target/debug/command_line_app.d
@@ -0,0 +1,1 @@
+/Users/roski1/RustApp/command_line_app/target/debug/command_line_app: /Users/roski1/RustApp/command_line_app/src/main.rs
---
+++ command_line_app/target/debug/deps/command_line_app-a8e42600392bcb9e.d
@@ -0,0 +1,5 @@
+/Users/roski1/RustApp/command_line_app/target/debug/deps/command_line_app-a8e42600392bcb9e: src/main.rs
+
+/Users/roski1/RustApp/command_line_app/target/debug/deps/command_line_app-a8e42600392bcb9e.d: src/main.rs
+
+src/main.rs:
